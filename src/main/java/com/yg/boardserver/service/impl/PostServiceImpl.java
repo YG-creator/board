e866c0ebaa8744dda49655fbfa8ba863ea57870f
YG-creator <2016272046@yonsei.ac.kr>
2024-02-11 15:07:31 +0900

    enh: issue/4 게시판 API 개발(Post CRUD API)

---
+++ src/main/java/com/yg/boardserver/service/impl/PostServiceImpl.java
@@ -0,0 +1,68 @@
+package com.yg.boardserver.service.impl;
+
+import com.yg.boardserver.dto.PostDTO;
+import com.yg.boardserver.dto.UserDTO;
+import com.yg.boardserver.mapper.PostMapper;
+import com.yg.boardserver.mapper.UserProfileMapper;
+import com.yg.boardserver.service.PostService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Log4j2
+public class PostServiceImpl implements PostService {
+
+    private final PostMapper postMapper;
+
+    private final UserProfileMapper userProfileMapper;
+
+    @Autowired
+    public PostServiceImpl(PostMapper postMapper, UserProfileMapper userProfileMapper) {
+        this.postMapper = postMapper;
+        this.userProfileMapper = userProfileMapper;
+    }
+
+    @CacheEvict(value="getProducts", allEntries = true)
+    @Override
+    public void register(String id, PostDTO postDTO) {
+        UserDTO memberInfo = userProfileMapper.getUserProfile(id);
+        postDTO.setUserId(memberInfo.getId());
+        postDTO.setCreateTime(new Date());
+
+        if (memberInfo != null) {
+            postMapper.register(postDTO);
+        } else {
+            log.error("register ERROR! {}", postDTO);
+            throw new RuntimeException("register ERROR! 상품 등록 메서드를 확인해주세요\n" + "Params : " + postDTO);
+        }
+    }
+
+    @Override
+    public List<PostDTO> getMyProducts(int accountId) {
+        return postMapper.selectMyProducts(accountId);
+    }
+
+    @Override
+    public void updateProducts(PostDTO postDTO) {
+        if (postDTO != null && postDTO.getId() != 0 && postDTO.getUserId() != 0) {
+            postMapper.updateProducts(postDTO);
+        } else {
+            log.error("updateProducts ERROR! {}", postDTO);
+            throw new RuntimeException("updateProducts ERROR! 물품 변경 메서드를 확인해주세요\n" + "Params : " + postDTO);
+        }
+    }
+
+    @Override
+    public void deleteProduct(int userId, int productId) {
+        if (userId != 0 && productId != 0) {
+            postMapper.deleteProduct(productId);
+        } else {
+            log.error("deleteProudct ERROR! {}", productId);
+            throw new RuntimeException("updateProducts ERROR! 물품 삭제 메서드를 확인해주세요\n" + "Params : " + productId);
+        }
+    }
+}
